d3a0569901a986f8051fabc83b8e7ac7f362e84f
EspressoCode <hn.84@yahoo.com>
2018-10-10 11:24:21 -0400

    Update Player.cpp

--- Part-4/Player.cpp
+++ Part-4/Player.cpp
@@ -1,6 +1,11 @@
-#include "Maploader.h"
-#include "Map.h"
 #include "Player.h"
+#include "MapLoader.h"
+#include "Map.h"
+#include "Dice.h"
+#include "DiceFacility.h"
+
+#include <iostream>
+#include <string>
 usng namespace std;
 
 
