3b928c7d50f516b388f8ba187db96bcb3a95ce87
EspressoCode <hn.84@yahoo.com>
2018-10-10 11:26:27 -0400

    Update Player.cpp

--- Part-4/Player.cpp
+++ Part-4/Player.cpp
@@ -2,7 +2,9 @@
 #include "MapLoader.h"
 #include "Map.h"
 #include "Dice.h"
-#include "DiceFacility.h"
+#include "DiceFacility.h" 
+#include "collections.h"
+#include "constants.h"
 
 #include <iostream>
 #include <string>
