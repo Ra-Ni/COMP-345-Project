f8ee44001ae65f4a749bf643d40d7c8e73216b03
Ra-Ni <32001054+Ra-Ni@users.noreply.github.com>
2018-10-11 16:40:22 -0400

    deleted Map

---
+++ Part-1/graphmap.cpp
@@ -0,0 +1,62 @@
+#include "graphmap.h"
+#include <iostream>
+
+graphmap::graphmap(char size) {
+    nodes = new std::vector<char>[size];
+    this->size = size;
+    visited = new bool[size];
+    for (int i = 0; i < size; i++) {
+        *(visited + i) = false;
+    }
+}
+
+graphmap::~graphmap() {
+    delete[] nodes;
+    delete[] visited;
+    nodes = nullptr;
+    visited = nullptr;
+
+}
+
+void graphmap::link(char n1, char n2) {
+    nodes[n1].push_back(n2);
+}
+
+void graphmap::DFS(int start) {
+    visited[start] = true;
+    for (auto index : nodes[start]) {
+        if (!visited[index]) {
+            DFS(index);
+        }
+    }
+}
+
+bool graphmap::isGraphConnected() {
+    bool flag = false;
+    int i = 0;
+    while(!flag && i < size) {
+        DFS(i++);
+        for (char j = 0; j < size; j++) {
+            if (!*(visited + j)) {
+                flag = true;
+            }
+            *(visited + j) = false;
+        }
+    }
+
+    return !flag;
+
+}
+
+void graphmap::print() {
+    for (char i = 0; i < size; i++) {
+        std::cout << (int) i;
+        for (auto index : nodes[i]) {
+            std::cout << " -> " << (int) index;
+        }
+        std::cout << std::endl;
+    }
+}
+
+
+
---
+++ Part-1/graphmap.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <vector>
+
+
+class graphmap {
+    std::vector<char> *nodes;
+    bool *visited;
+    char size;
+    void DFS(int start);
+public:
+    explicit graphmap(char size);
+
+    ~graphmap();
+
+    void link(char n1, char n2);
+
+    bool isGraphConnected();
+
+    //todo erase
+    void print();
+};
