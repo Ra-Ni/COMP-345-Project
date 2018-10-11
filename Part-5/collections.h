03e8a944eaa917577ca1d7796e6a910dc78dbacc
Ra-Ni <32001054+Ra-Ni@users.noreply.github.com>
2018-10-11 16:46:58 -0400

    Final Version

---
+++ Part-5/collections.h
@@ -0,0 +1,31 @@
+#ifndef COMP_345_PROJECT_STRUCTURES_H
+#define COMP_345_PROJECT_STRUCTURES_H
+#endif
+
+
+struct monster {
+    char _id;
+    char _health;
+    char _victory_points;
+    bool _taken;
+};
+
+struct tile {
+    char _id;
+    char _type;
+    char _reward;
+    char _durability;
+    char _count;
+};
+
+struct token {
+    char _id;
+    char _count;
+};
+
+struct card {
+    char _id;
+    char _description;
+    char _effect;
+    char _cost;
+};
