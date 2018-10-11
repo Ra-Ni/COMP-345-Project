4db839fb095dc52587ab159d087c7373f055f450
Ra-Ni <32001054+Ra-Ni@users.noreply.github.com>
2018-10-11 16:42:11 -0400

    Updated

---
+++ Part-2/maploader.cpp
@@ -0,0 +1,64 @@
+#include "maploader.h"
+#include "../Part-1/graphmap.h"
+#include <fstream>
+
+using namespace std;
+
+maploader::maploader(std::string &pathname) {
+    this->path = pathname;
+}
+
+void maploader::read() {
+    std::ifstream in(path, std::ios::in);
+    if (in.fail()) {
+        std::cout << "Failed to open the file" << std::endl;
+        exit(-1);
+    }
+    bool flag[2] = {false, false};
+    std::string s;
+    while (!in.eof() && !(flag[0] && flag[1])) {
+        getline(in, s);
+        if (s == "<Territories>") {
+            flag[0] = true;
+        } else if (s == "<Map>") {
+            flag[1] = true;
+        } else if (!s.empty()) {
+            ID.push_back(s);
+        }
+    }
+
+    if (!(flag[0] && flag[1])) {
+        std::cout << "File is invalid" << std::endl;
+        exit(-1);
+    }
+
+    g = new graphmap((char) ID.size());
+    while (!in.eof()) {
+        getline(in, s);
+        if (s.empty()) {
+            continue;
+        }
+        char node = (char) (s.at(0) - 48);
+        s = s.substr(2, s.size());
+        for (char index : s) {
+            if (index - 48 > ID.size() || index - 48 < 0) {
+                std::cout << "File is invalid" << std::endl;
+            }
+            g->link(node, (char) (index - 48));
+        }
+    }
+    in.close();
+}
+
+void maploader::print() {
+    g->print();
+    int i = 0;
+    for (string &id : ID) {
+        std::cout << id << '(' << i++ << ')' << std::endl;
+    }
+}
+
+bool maploader::isConnected() {
+    return g->isGraphConnected();
+}
+
---
+++ Part-2/maploader.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+class graphmap;
+
+class maploader {
+    std::vector<std::string> ID;
+    std::string path;
+    graphmap *g;
+
+
+public:
+    maploader() = default;
+
+    explicit maploader(std::string &pathname);
+
+    void read();
+
+    void print();
+
+    bool isConnected();
+};
