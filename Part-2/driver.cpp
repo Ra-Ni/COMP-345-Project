856bb4ab071ea8a90b92af51dd1b84b8f5f5e577
Rani Rafid <ranii.rafid@gmail.com>
2018-10-14 04:08:18 -0400

    none

--- Part-2/driver.cpp
+++ Part-2/driver.cpp
@@ -14,7 +14,7 @@ using namespace std;
 namespace part2 {
     int main() {
         cout << "Creating string path to King Of New York.map" << endl;
-        string s = "../Resources/King Of New York.map";
+        string s = "./Resources/King Of New York.map";
         cout << "Creating maploader object containing file path" << endl;
         maploader m(s);
         cout << "Reading the file" << endl;
@@ -22,7 +22,7 @@ namespace part2 {
         cout << "Adjacency map:\n" << m.to_string();
         cout << "is map connected? " << (m.isConnected() ? "Yes" : "No") << endl;
         cout << "\n\nCreating new maploader object, with missing node 5 for all except node 6" << endl;
-        s = "../Resources/bad.map";
+        s = "./Resources/bad.map";
         maploader m2(s);
         cout << "Reading the file" << endl;
         m2.read();
